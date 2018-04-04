cdcb7504a2c3d03b3793d8450df55eb78851e5b8
Brea Borlas and Garnett Puett <brea@>
2018-04-04 09:20:38 -0700

    Add player constructor, ability to roll and accumulate points in turn point total using prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
+function Player(name){
+  this.name = name;
+  this.totalPoints = 0;
+  this.turnPoints = 0;
+}
+
+Player.prototype.addRollPoints = function (points) {
+  this.turnPoints += points;
+};
+
+var player1 = new Player("Garnett");
+
 function rollDice(){
   return (Math.floor(Math.random() * 6)) + 1;
 }
 
 
-
-
-
-
 $(document).ready(function() {
   $("#roll").click(function(event) {
     event.preventDefault();
-    console.log(rollDice());
+    var rollPoints = rollDice();
+    player1.addRollPoints(rollPoints);
+    console.log(player1);
   });
 });
