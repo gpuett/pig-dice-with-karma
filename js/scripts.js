e327384de1076687167bcc8b76b3e0def13b15c1
Brea Borlas and Garnett Puett <garnett@>
2018-04-04 10:07:02 -0700

    Add organization, end of turn conditions

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,53 @@ Player.prototype.addRollPoints = function (points) {
   this.turnPoints += points;
 };
 
-var player1 = new Player("Garnett");
+function endTurnDecider() {
+  this.status = false;
+}
 
 function rollDice(){
   return (Math.floor(Math.random() * 6)) + 1;
+}
+
+var player1 = new Player("Garnett");
+var player2 = new Player("Brea");
+var endTurn = new endTurnDecider();
+
+var currentPlayer = player1;
+
+function play(){
+  var rollPoints = rollDice();
+  console.log(rollPoints);
+  if (rollPoints === 1) {
+    endTurn.status = true;
+    currentPlayer.turnPoints = 0;
+  }
+  if (endTurn.status === true) {
+    console.log("end");
+    currentPlayer.totalPoints += currentPlayer.turnPoints;
+    currentPlayer.turnPoints = 0;
+    console.log(currentPlayer);
+    if (currentPlayer === player1){
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+    endTurn.status = false;
+  }
+  else {
+    currentPlayer.addRollPoints(rollPoints);
+    console.log(currentPlayer);
+  }
 }
 
 
 $(document).ready(function() {
   $("#roll").click(function(event) {
     event.preventDefault();
-    var rollPoints = rollDice();
-    player1.addRollPoints(rollPoints);
-    console.log(player1);
+    play();
+  });
+  $("#hold").click(function(event) {
+    event.preventDefault();
+    endTurn.status = true;
   });
 });
